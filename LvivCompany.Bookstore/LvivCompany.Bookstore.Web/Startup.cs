78fa1e77dc9de96dbb3551bd3cdd6fbdd13cee73
Mykola Kos <kosmykola@gmail.com>
2017-07-11 16:45:35 +0300

    Add json file with settings (dbConectionsstrings), add Identity and DbContext for IIdentity

--- LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using LvivCompany.Bookstore.Entities;
 
 namespace LvivCompany.Bookstore.Web
 {
@@ -14,20 +19,31 @@ namespace LvivCompany.Bookstore.Web
     {
         public IConfiguration Configuration { get; }
 
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionToIdentityDb")));
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,7 +55,9 @@ namespace LvivCompany.Bookstore.Web
             }
 
             app.UseStaticFiles();
-
+#pragma warning disable CS0618 // Тип или член устарел
+            app.UseIdentity();
+#pragma warning restore CS0618 // Тип или член устарел
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
