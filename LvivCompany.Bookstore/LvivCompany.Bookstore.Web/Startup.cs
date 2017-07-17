354cd026b09ca4dfd7d9a8b808c678852f74ff72
Torbins <mikolabrody@ukr.net>
2017-07-17 16:06:14 +0300

    Build fixes

--- LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
@@ -30,7 +30,7 @@ namespace LvivCompany.Bookstore.Web
             Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
