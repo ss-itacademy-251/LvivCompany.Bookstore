02b6df42637651d17e694afb7d63681776e7318c
DESKTOP-1JBBTFH\Kostia <chumakostya@gmail.com>
2017-07-18 10:02:08 +0300

    Changed AddBookView, added BookViewModel

---
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/ViewModels/BookViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LvivCompany.Bookstore.Web.ViewModels
+{
+    public class BookViewModel
+    {
+        public string Name { get; set; }
+        public DateTime Year { get; set; }
+        [Display(Name = "Number of pages")]
+        public int NumberOfPages { get; set; }
+        [Display(Name = "Number of items")]
+        public int Amount { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
