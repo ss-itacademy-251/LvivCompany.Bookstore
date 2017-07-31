1838fc2d0baf803d40d20f5109c7ab2f149732de
DESKTOP-1JBBTFH\Kostia <chumakostya@gmail.com>
2017-07-31 14:17:17 +0300

    Removed hardcoded text for labels for authors

--- LvivCompany.Bookstore/LvivCompany.Bookstore.Web/ViewModels/AuthorViewModel.cs
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/ViewModels/AuthorViewModel.cs
@@ -5,9 +5,11 @@ namespace LvivCompany.Bookstore.Web.ViewModels
     public class AuthorViewModel
     {
         [Required(ErrorMessage = "The author's first name is required")]
+        [Display (Name ="First Name") ]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "The author's last name is required")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
     }
 }
