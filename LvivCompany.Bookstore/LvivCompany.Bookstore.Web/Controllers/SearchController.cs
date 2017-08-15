afb2e5444ed879906d4dab41f09d707df01bc672
AnnaAvramenko <avramenko.anychka@gmail.com>
2017-08-16 00:00:07 +0300

    Search by title and author

---
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LvivCompany.Bookstore.DataAccess.Repo;
+using LvivCompany.Bookstore.Entities;
+using LvivCompany.Bookstore.Web.Mapper;
+using LvivCompany.Bookstore.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LvivCompany.Bookstore.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IRepo<Book> _bookRepo;
+        private IRepo<Author> _authorRepo;
+        private IMapper<Book, BookViewModel> _bookmapper;
+
+        public SearchController(IRepo<Book> bookRepo, IRepo<Author> authorRepo, IMapper<Book, BookViewModel> bookmapper)
+        {
+            _bookRepo = bookRepo;
+            _authorRepo = authorRepo;
+            _bookmapper = bookmapper;
+        }
+
+        public async Task<IActionResult> Index(string SearchText)
+        {
+            List<Book> books = (await _bookRepo.GetAllAsync()).ToList();
+            if (!string.IsNullOrEmpty(SearchText))
+            {
+                var result = books.Where(s => (s.Name.ToLower().Contains(SearchText.ToLower())
+                || (s.BookAuthors.Select(x => x.Author).Any(v => (v.FirstName.ToLower().Contains(SearchText.ToLower()) || v.LastName.ToLower().Contains(SearchText.ToLower()))))));
+                return View(new HomePageListViewModel() { Books = _bookmapper.Map(result.ToList()) });
+            }
+            return View(new HomePageListViewModel() { Books = _bookmapper.Map(books) });
+        }
+    }
+}
