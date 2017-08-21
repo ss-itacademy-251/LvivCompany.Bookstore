63587c218632e3426b875c841af83430b6f8a4dc
DESKTOP-1JBBTFH\Kostia <chumakostya@gmail.com>
2017-08-21 13:38:28 +0300

    Add staging environment

---
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Extensions/AzureKeyVaultExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace LvivCompany.Bookstore.Web
+{
+    public static class AzureKeyVaultExtensions
+    {
+        public static IConfigurationBuilder AddAzureKeyVaults(this IConfigurationBuilder builder, IHostingEnvironment env, IConfiguration config)
+        {
+            if (env.IsStaging())
+            {
+                builder.AddAzureKeyVault(
+                   $"https://{config["azureKeyVaultforDB:vault"]}.vault.azure.net/",
+                   config["azureKeyVaultforDB:clientId"],
+                   config["azureKeyVaultforDB:clientSecret"]);
+            }
+
+            builder.AddAzureKeyVault(
+            $"https://{config["azureKeyVault:vault"]}.vault.azure.net/",
+            config["azureKeyVault:clientId"],
+            config["azureKeyVault:clientSecret"]);
+            return builder;
+        }
+    }
+}
--- LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
+++ LvivCompany.Bookstore/LvivCompany.Bookstore.Web/Startup.cs
@@ -5,18 +5,18 @@ using LvivCompany.Bookstore.Web.Mapper;
 using LvivCompany.Bookstore.Web.ViewModels;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using Microsoft.AspNetCore.Identity;
 
 namespace LvivCompany.Bookstore.Web
 {
     public class Startup
     {
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
         public Startup(IHostingEnvironment env)
         {
@@ -27,20 +27,14 @@ namespace LvivCompany.Bookstore.Web
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             var config = builder.Build();
-            builder.AddAzureKeyVault(
-            $"https://{config["azureKeyVault:vault"]}.vault.azure.net/",
-            config["azureKeyVault:clientId"],
-            config["azureKeyVault:clientSecret"]);
-
+            builder.AddAzureKeyVaults(env, config);
             Configuration = builder.Build();
+            Environment = env;
         }
 
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionToIdentityDb")));
-
-
+            services.AddDbContexts(Environment, Configuration);
             services.AddIdentity<User, IdentityRole<long>>(o =>
             {
                 o.Password.RequireNonAlphanumeric = false;
@@ -49,27 +43,21 @@ namespace LvivCompany.Bookstore.Web
             })
                .AddEntityFrameworkStores<ApplicationContext>()
                .AddDefaultTokenProviders();
-
             services.AddScoped<RoleManager<IdentityRole<long>>, RoleManager<IdentityRole<long>>>();
-
             services.AddMvc();
-
-            services.AddDbContext<BookStoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionToDb")));
-
-            services.AddTransient<IRepo<Book>, BookRepository>();
-            services.AddTransient<IRepo<Author>, AuthorRepository>();
-            services.AddTransient<IRepo<Category>, CategoryRepository>();
-            services.AddTransient<IRepo<Order>, OrderRepository>();
-            services.AddTransient<IRepo<OrderDetail>, OrderDetailRepository>();
-            services.AddTransient<IRepo<Publisher>, PublisherRepository>();
-            services.AddTransient<IRepo<Status>, StatusRepository>();
-
-            services.AddTransient<IMapper<Book, BookViewModel>, BookMapper>();
-            services.AddTransient<IMapper<Book, EditBookViewModel>, EditBookMapper>();
+            services.AddScoped<IRepo<Book>, BookRepository>();
+            services.AddScoped<IRepo<Author>, AuthorRepository>();
+            services.AddScoped<IRepo<Category>, CategoryRepository>();
+            services.AddScoped<IRepo<Order>, OrderRepository>();
+            services.AddScoped<IRepo<OrderDetail>, OrderDetailRepository>();
+            services.AddScoped<IRepo<Publisher>, PublisherRepository>();
+            services.AddScoped<IRepo<Status>, StatusRepository>();
+            services.AddScoped<IMapper<Book, BookViewModel>, BookMapper>();
+            services.AddScoped<IMapper<Book, EditBookViewModel>, EditBookMapper>();
             services.AddSingleton(Configuration);
-            services.AddTransient<IMapper<User, EditProfileViewModel>, ProfileMapper>();
-            services.AddTransient<IMapper<User, RegisterViewModel>, RegisterMapper>();
-
+            services.AddSingleton(Environment);
+            services.AddScoped<IMapper<User, EditProfileViewModel>, ProfileMapper>();
+            services.AddScoped<IMapper<User, RegisterViewModel>, RegisterMapper>();
             var serviceProvider = services.BuildServiceProvider();
             var context = serviceProvider.GetService<BookStoreContext>();
             DbInitializer.Seed(context);
@@ -80,7 +68,6 @@ namespace LvivCompany.Bookstore.Web
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage(); 
@@ -90,11 +77,10 @@ namespace LvivCompany.Bookstore.Web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();      
 
+            app.UseStaticFiles();      
             app.UseAuthentication();
             IdentityDbInitializer.Initialize(app.ApplicationServices, Configuration);
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
